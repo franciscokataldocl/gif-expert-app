ad0e0f72a54b8b178b8836b33eee5875f262f15d
franciscokataldocl <franciscolork@gmail.com>
2021-07-04 03:32:15 -0400

    modificado url fetch gifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,18 +1,19 @@
-export const getGifs = async(category)=>{
-    const apiKey = 'NaLulphlQpKOzfp5zxNP0hgrMdvOTEe1';
-    const url =`http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`;
-    const resp = await fetch(url);
-    const {data} = await resp.json();
-    //console.log(data)
-    const gifs = data.map(img=>{
-        return{
-            id: img.id,
-            title: img.title,
-            url: img.images?.downsized_medium.url
-        }
-    })
-    // console.log(gifs);
-return gifs;
-    // setImages(gifs);
-    
-}
+export const getGifs = async (category) => {
+  const apiKey = "NaLulphlQpKOzfp5zxNP0hgrMdvOTEe1";
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=${apiKey}`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  //console.log(data)
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+  // console.log(gifs);
+  return gifs;
+  // setImages(gifs);
+};
